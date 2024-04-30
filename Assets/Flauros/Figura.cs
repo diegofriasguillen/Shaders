31a2f8d8019c4096cc3ebd651647d2554ac9e03e
diegofriasguillen <diego.frias@artek.edu.mx>
2024-04-30 08:48:08 -0600

    hacer cubito girar

--- Assets/Flauros/Figura.cs
+++ Assets/Flauros/Figura.cs
@@ -10,52 +10,35 @@ public class Figura : MonoBehaviour
 {
     public Material material;
     Vector3[] vertices = {
-         
-                           //Base Octaedro
-                           new Vector3(0, 0, 0), //0
-                           new Vector3(1, 0, 0), //1
-                           new Vector3(0, 0, 1), //2
-                           new Vector3(1, 0, 1),  //3
-                           //puntas
-                           new Vector3(0.5f, 1, 0.5f), //4
-                           new Vector3(0.5f, -1, 0.5f)  //5
+
 
                            //Cube
-                           //new Vector3(0, 0, 0), //0
-                           //new Vector3(0, 1, 0), //1
-                           //new Vector3(1, 1, 0), //2
-                           //new Vector3(1, 0, 0), //3
-                           //new Vector3(0, 1, 1), //4
-                           //new Vector3(0, 0, 1), //5
-                           //new Vector3(1, 1, 1), //6
-                           //new Vector3(1, 0, 1)  //7
+                           new Vector3(0, 0, 0), //0
+                           new Vector3(0, 1, 0), //1
+                           new Vector3(1, 1, 0), //2
+                           new Vector3(1, 0, 0), //3
+                           new Vector3(0, 1, 1), //4
+                           new Vector3(0, 0, 1), //5
+                           new Vector3(1, 1, 1), //6
+                           new Vector3(1, 0, 1)  //7
                                                     };
 
-    int[] triangles = { 
-        
-                        //Octaedro
-                        0,4,1,
-                        2,4,0,
-                        1,4,3,
-                        3,4,2,
-                        0,1,5,
-                        2,0,5,
-                        1,3,5,
-                        3,2,5,
-                        
+    int[] triangles = {
+
+
                         //Cube
-                        //0, 1, 2,
-                        //0, 2, 3,
-                        //1, 0, 5,
-                        //1, 5, 4,
-                        //2, 6, 3,
-                        //6, 7, 3,
-                        //1, 4, 2,
-                        //4, 6, 2,
-                        //5, 0, 3,
-                        //5, 3, 7,
-                        //4, 5, 7,
-                        //4, 7, 6
+                        0, 1, 2,
+                        0, 2, 3,
+                        1, 0, 5,
+                        1, 5, 4,
+                        2, 6, 3,
+                        6, 7, 3,
+                        1, 4, 2,
+                        4, 6, 2,
+                        5, 0, 3,
+                        5, 3, 7,
+                        4, 5, 7,
+                        4, 7, 6
                         };
 
 
--- Assets/Matrices/TestMatrices.cs
+++ Assets/Matrices/TestMatrices.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class TestMatrices : MonoBehaviour
 {
+    Matriz modelMatriz = new Matriz();
+
+    public GameObject cube;
     void Start()
     {
         
     }
-    void Update()
+    //private void FixedUpdate()
+    //{
+    //    cube.transform.position = modelMatriz.Traslation(cube.transform.position, new Vector3(0.00f, 0, 0.02f));
+
+    //}
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.position = modelMatriz.RotZ(this.transform.position, 30);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.position = modelMatriz.RotX(this.transform.position, 30);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.position = modelMatriz.RotY(this.transform.position, 30);
+        }
+
     }
 }
